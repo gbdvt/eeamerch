e00c5099dc8625fbfb80ee6f5755b77886fde7ed
Gaspar B. Development <70714917+gbdvt@users.noreply.github.com>
2022-12-06 23:51:09 +0100

    fix?

--- front-end/src/Containers/FrontPage/FrontPage.js
+++ front-end/src/Containers/FrontPage/FrontPage.js
@@ -19,7 +19,7 @@ const FrontPage = () => {
     return (
         <div>
             <div className={styles.textContainer}>
-                <p>Please order by clicking on "store" in the top right corner.<br></br>Pay at school by April 7th. <br></br>Collect at school after the holidays.</p>
+                <p>Presale right now, click on the "store" button in the top right corner to purchase your hoodie!</p>
             </div>
         </div>
     )
